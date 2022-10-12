ce4e4dbfed37c4b02364f5b52206bd21ec4f0fcc
Karuna Mishra <karunamishra25@gmail.com>
2022-10-12 18:01:56 +0545

    main work done

---
+++ maincalc.py
@@ -0,0 +1,14 @@
+import add_work
+import sub_work
+import mul_work
+import div_wrk
+
+a=100
+b=50
+
+total_sum=add_work.add(a,b)
+total_sub=sub_work.sub(a,b)
+total_mul=mul_work.mul(a,b)
+total_div=div_wrk.div(a,b)
+
+print(f"Total sum ={total_sum}. Total sub={total_sub}. Total mul={total_mul}. Total div={total_div}")
